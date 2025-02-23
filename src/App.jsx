8cf458b33d65c8f1d70c33fe5f03aa3915e1576a
Diyorbek G'aybullayev <gaybullayevdiyorbek2001@gmail.com>
2025-02-23 15:01:48 +0500

    add file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-[50px] text-amber-400 hover:text-[red] '>Salom</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
